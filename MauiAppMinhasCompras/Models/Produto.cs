e669be0c0add2a23b85d0c5a4165e09c690b1771
angelicabeli <angelica.rolle@etec.sp.gov.br>
2025-03-23 00:47:49 -0300

    Validacao da insercao de produtos e correcao dos produtos duplicados na tela inicial.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,10 +9,22 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
+        public string Descricao { 
+            get => _descricao; 
+            set 
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descricao");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade {get; set;}
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
 
