a5dadb0498450d31a5e3961d20499b3aa43ab8d5
angelicabeli <angelica.rolle@etec.sp.gov.br>
2025-03-28 01:47:45 -0300

    agenda 06 DS

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,12 @@
 ﻿using MauiAppMinhasCompras.Models;
 using SQLite;
+
 namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
         readonly SQLiteAsyncConnection _conn;
+
         public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
@@ -18,13 +20,13 @@ namespace MauiAppMinhasCompras.Helpers
 
         public Task<List<Produto>> Update(Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
-
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=?, Categoria=? WHERE Id=?";
             return _conn.QueryAsync<Produto>(
-                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Categoria, p.Id
             );
         }
 
+       
         public Task<int> Delete(int id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
@@ -35,11 +37,18 @@ namespace MauiAppMinhasCompras.Helpers
             return _conn.Table<Produto>().ToListAsync();
         }
 
-        public Task<List<Produto>> Search(string q)
+        public Task<List<Produto>> Searchdescricao(string q)
         {
             string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
-
             return _conn.QueryAsync<Produto>(sql);
+                        
+        }
+        public Task<List<Produto>> Searchcategoria(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE categoria LIKE '%" + q + "%'";
+            return _conn.QueryAsync<Produto>(sql);
+            
         }
     }
 }
+
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -50,13 +50,18 @@ public partial class ListaProduto : ContentPage
 
 
             string q = e.NewTextValue;
-        lista.Clear();
-        List<Produto> tmp = await App.Db.Search(q);
+            lst_produtos.IsRefreshing = true;
+            lista.Clear();
+        List<Produto> tmp = await App.Db.Searchdescricao(q);
      tmp.ForEach(i => lista.Add(i));
     }
         catch (Exception ex) 
         {
            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
     }
 
@@ -108,5 +113,45 @@ public partial class ListaProduto : ContentPage
             DisplayAlert("Ops", ex.Message, "OK");
         }
     }
+
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+            List<Produto> tmp = await App.Db.GetAll();
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }finally
+         
+        {
+            lst_produtos.IsRefreshing = false;
+        }
 }
+
+    private async void txt_busca_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string cat = e.NewTextValue;
+            lst_produtos.IsRefreshing = true;
+            lista.Clear();
+            List<Produto> tmp = await App.Db.Searchcategoria(cat);
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
+        }
+    }
+
+}
+
 
